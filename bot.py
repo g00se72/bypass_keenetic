b9a3eb622995ebb45247b2fefca66ad19a96f0f9
Eugen O. <147696077+g00se72@users.noreply.github.com>
2025-02-26 13:24:48 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -109,7 +109,7 @@ def bot_message(message):
 
             if message.text in ('🤖 Перезапустить бота','Перезапустить бота'):
                 bot.send_message(message.chat.id, "⏳ Бот будет перезапущен", reply_markup=service)
-                subprocess.Popen(['/opt/root/script.sh', '-restart'])
+                subprocess.Popen(['/opt/root/script.sh', '-restart'], close_fds=True)
                 return
             
             if message.text in ('⁉️ DNS Override','DNS Override'):
