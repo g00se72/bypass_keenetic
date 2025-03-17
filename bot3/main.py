35c328b2e1a9354d04e196101e989dcabb58fa50
Eugen O. <147696077+g00se72@users.noreply.github.com>
2025-03-17 23:44:40 +0500

    Update main.py

--- bot3/main.py
+++ bot3/main.py
@@ -11,13 +11,20 @@ bot = telebot.TeleBot(config.token)
 # Глобальные переменные
 level = 0
 selected_file = ""
+MAX_RESTARTS = 5
+restart_count = 0
 
 if __name__ == "__main__":
     setup_handlers(bot, level, selected_file)
-    while True:
+
+    while restart_count < MAX_RESTARTS:
         try:
             bot.infinity_polling()
         except Exception as err:
             with open(config.paths["error_log"], "a") as fl:
-                fl.write(f"{str(err)}\n")
-            time.sleep(300)  # Пауза перед перезапуском
+                fl.write(f"Critical Error: {str(err)}\n")
+            restart_count += 1
+            time.sleep(60)
+    
+    with open(config.paths["error_log"], "a") as fl:
+        fl.write("Bot stopped after reaching max restart attempts.\n")
