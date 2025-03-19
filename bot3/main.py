af9bf3d746706873f0eac6109d3c62d2550b6974
Eugen O. <147696077+g00se72@users.noreply.github.com>
2025-03-19 11:32:30 +0500

    Update main.py

--- bot3/main.py
+++ bot3/main.py
@@ -6,7 +6,7 @@ import sys
 import time
 import telebot
 from handlers import setup_handlers
-from utils import log_error
+from utils import log_error, write_pid, cleanup_pid
 import bot_config as config
 
 bot = telebot.TeleBot(config.token)
@@ -15,39 +15,6 @@ level = 0
 restart_count = 0
 selected_file = ""
 
-# Функция для записи PID в файл
-def write_pid(pid_file):
-    try:
-        pid = os.getpid()
-        if os.path.exists(pid_file):
-            with open(pid_file, "r") as f:
-                existing_pid = f.read().strip()
-                try:
-                    os.kill(int(existing_pid), 0)
-                    log_error(f"Ошибка: бот с PID {existing_pid} уже запущен")
-                    return False
-                except ProcessLookupError:
-                    log_error(f"Процесс с PID {existing_pid} не найден, возможно, бот не работает")
-                    os.remove(pid_file)
-
-        with open(pid_file, "w") as f:
-            f.write(str(pid))
-        log_error(f"PID {pid} записан в файл {pid_file}")
-        return True
-    except Exception as e:
-        log_error(f"Ошибка при записи в файл PID: {e}")
-        return False
-
-# Функция для очистки файла PID при завершении работы
-def cleanup_pid(pid_file):
-    try:
-        if os.path.exists(pid_file):
-            os.remove(pid_file)
-            log_error(f"Файл PID удален: {pid_file}")
-        else:
-            log_error(f"PID файл не найден для удаления: {pid_file}")
-    except Exception as e:
-        log_error(f"Ошибка при удалении PID файла: {e}")
 
 if __name__ == "__main__":
     # Проверяем можно ли запустить бот
