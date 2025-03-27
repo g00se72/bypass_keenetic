1d372c12c089c274f5c313e6b74116acede8ab56
Eugen O. <147696077+g00se72@users.noreply.github.com>
2025-03-27 18:40:15 +0500

    Update 100-ipset.sh

--- 100-ipset.sh
+++ 100-ipset.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
+
 [ "$1" != "start" ] && exit 0
 ipset create unblocksh hash:net -exist
 ipset create unblocktor hash:net -exist
-ipset create unblockvmess hash:net -exist
+ipset create unblockvless hash:net -exist
 ipset create unblocktroj hash:net -exist
-# ipset create unblockvpn hash:net -exist
 
 if ls -d /opt/etc/unblock/vpn-*.txt >/dev/null 2>&1; then
 for vpn_file_names in /opt/etc/unblock/vpn-*; do
@@ -14,14 +14,4 @@ ipset create "$unblockvpn" hash:net -exist
 done
 fi
 
-#script0
-#script1
-#script2
-#script3
-#script4
-#script5
-#script6
-#script7
-#script8
-#script9
 exit 0
