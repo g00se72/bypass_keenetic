07c80acd8952ac33338adc981dea11a492896c2c
Eugen O. <147696077+g00se72@users.noreply.github.com>
2025-09-09 23:23:48 +0500

    Update bot_config.py

--- botlight/bot_config.py
+++ botlight/bot_config.py
@@ -1,18 +1,18 @@
 # Настройки бота
-token = 'MyBotFatherToken'  # Ключ api бота
-usernames = ['MySuperLogin']  # Ваш логин в телеграмме без @
+token = "MyBotFatherToken"  # Ключ api бота
+usernames = ["MySuperLogin"]  # Ваш логин в телеграмме без @
 MAX_RESTARTS = 5  # Максимальное количество перезапусков бота
 RESTART_DELAY = 60  # Задержка в секундах перед перезапуском после ошибки
 
 # Настройки Proxy
 proxy0port = 9050  # Локальный порт для Tor socks5 прокси
-proxy0interface = Proxy0  # Название интерфейса
+proxy0interface = "Proxy0"  # Название интерфейса
 proxy1port = 1080  # Локальный порт для xray/sing-box
-proxy1interface = Proxy1  # Название интерфейса
+proxy1interface = "Proxy1"  # Название интерфейса
 
 # Настройки клиента vless
-vless_client = sing-box # Клиент для установки sing-box или xray
-client_mode = socks5 # Режим работы sing-box tun (с gvisor) или socks5, для xray - всегда socks5
+vless_client = "sing-box" # Клиент для установки sing-box или xray
+client_mode = "socks5" # Режим работы sing-box tun (с gvisor) или socks5, для xray - всегда socks5
 
 # Список пакетов
 packages = [
