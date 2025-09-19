7daae8fb98f716875297977a01e57df89542cb09
Eugen O. <147696077+g00se72@users.noreply.github.com>
2025-09-19 12:25:22 +0500

    Update script.sh

--- botlight/script.sh
+++ botlight/script.sh
@@ -185,10 +185,10 @@ elif [ "$1" = "-update" ]; then
 	
     # Что нужно обновить
     curl -s -o "$BOT_DIR/main.py" "$BOT_URL/main.py" || exit 1
-    curl -s -o "$BOT_DIR/menu.py" "$BOT_URL/menu.py" || exit 1
+    # curl -s -o "$BOT_DIR/menu.py" "$BOT_URL/menu.py" || exit 1
     curl -s -o "$BOT_DIR/utils.py" "$BOT_URL/utils.py" || exit 1
-    curl -s -o "$BOT_DIR/handlers.py" "$BOT_URL/handlers.py" || exit 1
-    curl -s -o "$INIT_BOT" "$BOT_URL/S99telegram_bot" || exit 1
+    # curl -s -o "$BOT_DIR/handlers.py" "$BOT_URL/handlers.py" || exit 1
+    # curl -s -o "$INIT_BOT" "$BOT_URL/S99telegram_bot" || exit 1
     # curl -s -o "$SCRIPT_BU" "$BASE_URL/KeenSnap/keensnap.sh" || exit 1
 	# curl -s -o "$TEMPLATES_DIR/tor_template.torrc" "$BOT_URL/tor_template.torrc" || exit 1
     # cp "$TEMPLATES_DIR/tor_template.torrc" "$TOR_CONFIG" && echo "Базовые настройки Tor обновлены"
@@ -196,7 +196,7 @@ elif [ "$1" = "-update" ]; then
     echo "Обновления для бота загружены, применяем права"
     chmod 755 "$BOT_DIR"
     chmod 644 "$BOT_DIR"/*.py
-	chmod 755 "$INIT_BOT" || chmod +x "$INIT_BOT"
+	# chmod 755 "$INIT_BOT" || chmod +x "$INIT_BOT"
     # chmod 755 "$SCRIPT_BU"
 
     # "$INIT_SINGBOX" restart > /dev/null 2>&1 || echo "❕Sing-box не запустился, проверьте конфигурацию"
