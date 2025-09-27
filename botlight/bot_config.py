ba636b87a506416667edb9bd16e223c46af7eb67
Eugen O. <147696077+g00se72@users.noreply.github.com>
2025-09-27 12:03:00 +0500

    Update bot_config.py

--- botlight/bot_config.py
+++ botlight/bot_config.py
@@ -6,9 +6,9 @@ RESTART_DELAY = 60  # Задержка в секундах перед перез
 
 # Настройки Proxy
 proxy0port = 9050  # Локальный порт для Tor socks5 прокси
-proxy0interface = "Proxy0"  # Название интерфейса для Tor
+proxy0interface = "Proxy0"  # Название интерфейса
 proxy1port = 1080  # Локальный порт для xray/singbox
-proxy1interface = "Proxy1"  # Название интерфейса для xray и singbox в режиме socks5
+proxy1interface = "Proxy1"  # Название интерфейса (для xray и singbox в режиме socks5)
 
 # Настройки клиента vless
 vless_client = "singbox" # Клиент для установки singbox или xray
@@ -26,41 +26,38 @@ packages = [
     "coreutils-split"
 ]
 
-# Базовые пути
+# Пути к конфигурационным файлам и директориям
 paths = {
+    "bot_config": "/opt/etc/bot/bot_config.py",
+    "tor_config": "/opt/etc/tor/torrc",
+    "singbox_config": "/opt/etc/sing-box/config.json",
+    "xray_config": "/opt/etc/xray/config.json",
+    "error_log": "/opt/etc/bot/error.log",
+    "keensnap_log": "/opt/root/KeenSnap/backup.log",
+    "keensnap_path": "/opt/root/KeenSnap/keensnap.sh",
+    "bot_path": "/opt/etc/bot/main.py",
+    "script_sh": "/opt/root/script.sh",
+    "chat_id_path": "/opt/var/run/bot_chat_id.txt",
+    "init_singbox": "/opt/etc/init.d/S99sing-box",
+    "init_xray": "/opt/etc/init.d/S24xray",
+    "init_tor": "/opt/etc/init.d/S35tor",
+    "init_bot": "/opt/etc/init.d/S99telegram_bot",
+    "init_MT": "/opt/etc/init.d/S99magitrickle",
     "bot_dir": "/opt/etc/bot",
-    "tor_dir": "/opt/etc/tor", 
-    "singbox_dir": "/opt/etc/sing-box",
-    "xray_dir": "/opt/etc/xray",
-    "keensnap_dir": "/opt/root/KeenSnap",
     "templates_dir": "/opt/etc/bot/templates",
-    "init_dir": "/opt/etc/init.d",
-    "chat_id_path": "/opt/var/run/bot_chat_id.txt"
+    "keensnap_dir": "/opt/root/KeenSnap",
+    "tor_dir": "/opt/etc/tor",
+    "singbox_dir": "/opt/etc/sing-box",
+    "xray_dir": "/opt/etc/xray"
 }
-
-# Динамическое создание путей на основе базовых путей
-paths["bot_path"] = paths["bot_dir"] + "/main.py"
-paths["bot_config"] = paths["bot_dir"] + "/bot_config.py"
-paths["error_log"] = paths["bot_dir"] + "/error.log"
-paths["script_sh"] = paths["bot_dir"] + "/script.sh"
-paths["keensnap_path"] = paths["keensnap_dir"] + "/keensnap.sh"
-paths["keensnap_log"] = paths["keensnap_dir"] + "/backup.log"
-paths["tor_config"] = paths["tor_dir"] + "/torrc"
-paths["singbox_config"] = paths["singbox_dir"] + "/config.json"
-paths["xray_config"] = paths["xray_dir"] + "/config.json"
-paths["init_bot"] = paths["init_dir"] + "/S99telegram_bot"
-paths["init_tor"] = paths["init_dir"] + "/S35tor"
-paths["init_singbox"] = paths["init_dir"] + "/S99sing-box"
-paths["init_xray"] = paths["init_dir"] + "/S24xray"
-paths["init_MT"] = paths["init_dir"] + "/S99magitrickle"
 
 # Команды для перезапуска сервисов
 services = {
-    "service_script": [paths["init_bot"], "restart"],
     "tor_restart": [paths["init_tor"], "restart"],
     "singbox_restart": [paths["init_singbox"], "restart"],
     "xray_restart": [paths["init_xray"], "restart"],
-    "MT_restart": [paths["init_MT"], "restart"]
+    "MT_restart": [paths["init_MT"], "restart"],
+    "service_script": [paths["init_bot"], "restart"]
 }
 
 # Базовые URL-адреса для скачиваемых файлов
@@ -73,10 +70,10 @@ backup_settings = {
     "LOG_FILE": paths["keensnap_log"],
     "MAX_SIZE_MB": 45,
     "CUSTOM_BACKUP_PATHS":" ".join([
-        # paths["singbox_config"],
+        paths["bot_dir"],
+        paths["singbox_config"],
         # paths["xray_config"],
-        # paths["tor_config"],
-        # paths["script_sh"],
-        paths["bot_dir"]
+        paths["tor_config"],
+        paths["script_sh"]
     ])
 }
